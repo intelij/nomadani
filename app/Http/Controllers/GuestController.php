dbccc05357241095774ddae8517732d668e639b3
Khululekani Mkhonza <ks.mkhonza@gmail.com>
2021-06-26 02:21:18 +0100

    update dates

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Guest;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class GuestController extends Controller
+{
+    public function index() {
+        return view('welcome', ['rsvp' => false]);
+    }
+
+    public function confirm(Request $request) {
+        return $request->all();
+    }
+
+    public function edit($phone)
+    {
+        $payload = [
+            'name' => 'Khululekani Mkhonza',
+            'phone' => '07983951875',
+            'confirmation' => false,
+        ];
+
+        return view('welcome', $payload);
+
+    }
+}
