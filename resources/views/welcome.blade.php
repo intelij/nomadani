b241e0a351a1a9d2c310c5dfe9a61cf41e6b86af
Khululekani Mkhonza <ks.mkhonza@gmail.com>
2021-07-02 17:54:59 +0100

    update contact details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -806,5 +806,14 @@
         });
     @endif
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-85220550-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-85220550-1');
+</script>
 </body>
 </html>
